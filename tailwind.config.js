499efb252e2f3a72198f47f3e99341f4ed5332f8
vipul-khedekar <3kvipul@gmail.com>
2022-09-09 20:42:48 +0530

    Update: Color scheme added.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        nero: "#242424",
+        choco: "#bc672c",
+        cream: "#ecddce",
+        sunny: "#eef4b1",
+        sea: "#acd7eb",
+        marine: "#00396a",
+      },
+    },
   },
   plugins: [],
 };
