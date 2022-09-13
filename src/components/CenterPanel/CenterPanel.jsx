cc02f160c3e3bc4f37e8138ed41ad0ae02f2e107
vipul-khedekar <3kvipul@gmail.com>
2022-09-13 18:51:15 +0530

    Fix: Slightly increased gap between cards.

--- src/components/CenterPanel/CenterPanel.jsx
+++ src/components/CenterPanel/CenterPanel.jsx
@@ -3,7 +3,7 @@ import Feed from "./Feed";
 
 function CenterPanel() {
   return (
-    <section className="flex flex-col flex-[2] justify-start items-center gap-4">
+    <section className="flex flex-col flex-[2] justify-start items-center gap-6">
       <Poster />
 
       <Feed />
--- src/components/RightPanel/RightPanel.jsx
+++ src/components/RightPanel/RightPanel.jsx
@@ -3,7 +3,7 @@ import AdoptionAlerts from "./AdoptionAlerts";
 
 function RightPanel() {
   return (
-    <section className="flex flex-col flex-[2] justify-start items-center gap-4">
+    <section className="flex flex-col flex-[2] justify-start items-center gap-6">
       <SearchBar />
 
       <AdoptionAlerts />
