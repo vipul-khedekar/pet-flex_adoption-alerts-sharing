722c3ebf15d6e977a237ecc0ad12bd946e751d8d
vipul-khedekar <3kvipul@gmail.com>
2022-09-13 23:19:55 +0530

    Update: Styled search bar.

--- src/components/RightPanel/SearchBar.jsx
+++ src/components/RightPanel/SearchBar.jsx
@@ -1,5 +1,17 @@
+import { IoMdSearch } from "react-icons/io";
+
 function SearchBar() {
-  return <div>SearchBar</div>;
+  return (
+    <article className="h-14 w-full flex justify-center items-center p-2 rounded-lg relative">
+      <input
+        className="w-full p-2 pl-8 rounded-md"
+        type="text"
+        placeholder="Search for specific adoption alerts"
+      />
+
+      <IoMdSearch className="absolute left-3 text-2xl" />
+    </article>
+  );
 }
 
 export default SearchBar;
