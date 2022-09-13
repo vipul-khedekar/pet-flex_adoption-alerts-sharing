749fd25d10e6730412672eeecc0b6e0d3dc73e61
vipul-khedekar <3kvipul@gmail.com>
2022-09-13 19:10:13 +0530

    Update: Styled Icons for options.

--- src/components/LeftPanel/Options.jsx
+++ src/components/LeftPanel/Options.jsx
@@ -1,7 +1,20 @@
+import { IoMdNotificationsOutline } from "react-icons/io";
+import {
+  IoLogOutOutline,
+  IoBookmarksOutline,
+  IoPersonOutline,
+} from "react-icons/io5";
+
 function Options() {
   return (
-    <article className="bg-cream h-16 w-64 flex justify-center items-center gap-2 rounded-lg">
-      Options
+    <article className="bg-cream h-16 w-64 flex justify-evenly items-center rounded-lg">
+      <IoPersonOutline className="text-2xl scale-100 ease-in duration-150 hover:scale-110" />
+
+      <IoBookmarksOutline className="text-2xl scale-100 ease-in duration-150 hover:scale-110" />
+
+      <IoMdNotificationsOutline className="text-2xl scale-100 ease-in duration-150 hover:scale-110" />
+
+      <IoLogOutOutline className="text-2xl scale-100 ease-in duration-150 hover:scale-110" />
     </article>
   );
 }
