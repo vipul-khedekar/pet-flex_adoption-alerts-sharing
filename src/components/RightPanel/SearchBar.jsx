81397739eca7070d5e3351cc8742451bd33f9eb0
vipul-khedekar <3kvipul@gmail.com>
2022-09-14 18:21:13 +0530

    Update: Added a show all button. Fix: Adjusted padding to not overlap the icon and button.

--- src/components/RightPanel/SearchBar.jsx
+++ src/components/RightPanel/SearchBar.jsx
@@ -4,12 +4,19 @@ function SearchBar() {
   return (
     <article className="bg-cream h-14 w-full flex justify-center items-center p-2 rounded-lg relative">
       <input
-        className="bg-sea w-full p-2 pl-8 rounded-md outline-none text-marine"
+        className="bg-sea w-full p-2 pl-[2.25rem] pr-[5.25rem] rounded-md outline-none text-marine"
         type="text"
         placeholder="Search for specific adoption alerts"
       />
 
-      <IoMdSearch className="absolute left-3 text-2xl" />
+      <IoMdSearch className="absolute left-3 z-10 text-2xl" />
+
+      <button
+        className="bg-marine text-sea text-sm px-2 py-1 rounded-md absolute z-10 right-3"
+        type="button"
+      >
+        Show All
+      </button>
     </article>
   );
 }
