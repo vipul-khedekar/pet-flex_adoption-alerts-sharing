6c844eb083325a1ca056842df95174a10a7b9c05
vipul-khedekar <3kvipul@gmail.com>
2022-09-14 17:35:06 +0530

    Fix: Click effect animations.

--- src/components/LeftPanel/Options.jsx
+++ src/components/LeftPanel/Options.jsx
@@ -7,14 +7,14 @@ import {
 
 function Options() {
   return (
-    <article className="bg-cream h-16 sm:w-60 lg:w-full flex justify-evenly items-center rounded-lg">
-      <IoPersonOutline className="text-2xl scale-100 ease-in duration-150 cursor-pointer hover:scale-110" />
+    <article className="bg-cream h-14 sm:w-60 lg:w-full flex justify-evenly items-center rounded-lg">
+      <IoPersonOutline className="text-2xl scale-100 ease-in duration-150 cursor-pointer hover:scale-110 active:translate-y-1" />
 
-      <IoBookmarksOutline className="text-2xl scale-100 ease-in duration-150 cursor-pointer hover:scale-110" />
+      <IoBookmarksOutline className="text-2xl scale-100 ease-in duration-150 cursor-pointer hover:scale-110 active:translate-y-1" />
 
-      <IoMdNotificationsOutline className="text-2xl scale-100 ease-in duration-150 cursor-pointer hover:scale-110" />
+      <IoMdNotificationsOutline className="text-2xl scale-100 ease-in duration-150 cursor-pointer hover:scale-110 active:translate-y-1" />
 
-      <IoLogOutOutline className="text-2xl scale-100 ease-in duration-150 cursor-pointer hover:scale-110" />
+      <IoLogOutOutline className="text-2xl scale-100 ease-in duration-150 cursor-pointer hover:scale-110 active:translate-y-1" />
     </article>
   );
 }
