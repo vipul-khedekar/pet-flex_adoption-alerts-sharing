0bd6628bdbf63f87d86b612360e58c5aa2450bc3
vipul-khedekar <3kvipul@gmail.com>
2022-09-14 16:50:12 +0530

    Update: Post styled with temporary placeholders.

--- src/components/CenterPanel/Feed.jsx
+++ src/components/CenterPanel/Feed.jsx
@@ -2,7 +2,7 @@ import Post from "../Post";
 
 function Feed() {
   return (
-    <article className="bg-cream h-[76vh] w-full flex flex-col justify-start items-center p-2 lg:p-4 rounded-lg">
+    <article className="bg-cream h-[76vh] w-full p-2 lg:p-4 rounded-lg overflow-y-scroll">
       <Post />
     </article>
   );
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,37 @@
+import DefaultProfilePic from "../images/profile-picture.jpg";
+
 function Post() {
-  return <div>Post</div>;
+  return (
+    <div className="flex flex-col gap-2 border-separate">
+      <div className="flex justify-start items-center gap-2">
+        <img
+          className="h-10 w-10 rounded-full object-cover"
+          src={DefaultProfilePic}
+          alt="profile-pic"
+        />
+
+        <div>
+          <p>Dummy Name</p>
+          <p className="text-sm">@username</p>
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center justify-start gap-2">
+        <p>
+          Hey, this is my cat. Look at my cat. This is the best cat in the
+          world. Do you like it? Please like and comment!
+        </p>
+
+        <img
+          className="h-40 w-full rounded-lg object-cover object-center"
+          src={DefaultProfilePic}
+          alt="content-pic"
+        />
+      </div>
+
+      <hr className="border-[0.5px] border-choco" />
+    </div>
+  );
 }
 
 export default Post;
