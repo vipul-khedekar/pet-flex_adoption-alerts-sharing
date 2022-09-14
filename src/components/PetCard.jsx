20db48854886e2bc38ddcfdadf3f8bd2daf2025a
vipul-khedekar <3kvipul@gmail.com>
2022-09-14 17:31:31 +0530

    Update: Pet info cards populated and styled with temporary content.

--- src/components/PetCard.jsx
+++ src/components/PetCard.jsx
@@ -1,5 +1,37 @@
+import PetPic from "../images/profile-picture.jpg";
+
 function PetCard() {
-  return <div>PetCard</div>;
+  return (
+    <div className="h-72 w-60 flex flex-col gap-2 border border-choco p-2 rounded-lg">
+      <img
+        className="h-64 w-56 rounded-lg object-cover"
+        src={PetPic}
+        alt="pet-pic"
+      />
+
+      <div className="flex justify-between items-center gap-4 px-4">
+        <p>Cat</p>
+
+        <p>Breed</p>
+      </div>
+
+      <div className="flex flex-col gap-2 px-4">
+        <button
+          className="bg-choco px-3 py-1 rounded-md text-sunny"
+          type="button"
+        >
+          Send message
+        </button>
+
+        <button
+          className="bg-choco px-3 py-1 rounded-md text-sunny"
+          type="button"
+        >
+          Bookmark
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default PetCard;
--- src/components/RightPanel/AdoptionAlerts.jsx
+++ src/components/RightPanel/AdoptionAlerts.jsx
@@ -2,7 +2,18 @@ import PetCard from "../PetCard";
 
 function AdoptionAlerts() {
   return (
-    <article className="bg-cream h-[87vh] w-full flex sm:flex-col lg:flex-wrap justify-start items-center p-2 lg:p-4 rounded-lg">
+    <article className="bg-cream h-[87vh] w-full flex flex-wrap justify-center gap-6 p-4 rounded-lg overflow-y-scroll">
+      <PetCard />
+      <PetCard />
+      <PetCard />
+      <PetCard />
+      <PetCard />
+      <PetCard />
+      <PetCard />
+      <PetCard />
+      <PetCard />
+      <PetCard />
+      <PetCard />
       <PetCard />
     </article>
   );
