209eac663e70ae9b1678930be607e833527d81fa
vipul-khedekar <3kvipul@gmail.com>
2022-09-16 18:53:22 +0530

    Fix: Remove Home and login routes as they depends on user loggin status.

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,7 +1,5 @@
 import { Routes, Route } from "react-router-dom";
 
-import Home from "./Home";
-import Login from "./Login";
 import Error from "./Error";
 import Signup from "./Signup";
 import Landing from "./Landing";
@@ -10,9 +8,7 @@ function Pages() {
   return (
     <Routes>
       <Route path={"/"} element={<Landing />} />
-      <Route path={"/login"} element={<Login />} />
       <Route path={"/signup"} element={<Signup />} />
-      <Route path={"/home"} element={<Home />} />
       <Route path={"/*"} element={<Error />} />
     </Routes>
   );
