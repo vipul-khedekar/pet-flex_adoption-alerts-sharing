7e45b8f89801d19bf69df8d0a1aa74104f441223
vipul-khedekar <3kvipul@gmail.com>
2022-09-16 19:46:10 +0530

    Update: Profile section will show signed users data.

--- src/components/LeftPanel/Profile.jsx
+++ src/components/LeftPanel/Profile.jsx
@@ -1,6 +1,8 @@
 import DefaultProfilePic from "../../images/profile-picture.jpg";
 
 function Profile() {
+  const user = JSON.parse(localStorage.getItem(`currentUser`));
+
   return (
     <article className="bg-cream h-72 sm:w-60 lg:w-full p-4 mt-24 rounded-lg relative">
       <img
@@ -10,13 +12,13 @@ function Profile() {
       />
 
       <div className="flex flex-col justify-center items-center gap-1 mt-36">
-        <p>Dummy Name</p>
+        <p className="text-xl">{user.username}</p>
 
-        <p className="text-sm">(Username)</p>
-
-        <p className="text-sm">Animal Lover</p>
+        <p className="text-sm">{`(@${user.tag})`}</p>
 
         <p className="mt-3 font-bold">Posts: {333}</p>
+
+        <p className="font-bold">Bookmarks: {12}</p>
       </div>
     </article>
   );
