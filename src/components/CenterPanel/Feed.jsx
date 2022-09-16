bc8a744835c28d9711d8ff4248cc5f178896b0a9
vipul-khedekar <3kvipul@gmail.com>
2022-09-16 21:33:24 +0530

    Fix: Feed section width responsive to medium and full browser size.

--- src/components/CenterPanel/Feed.jsx
+++ src/components/CenterPanel/Feed.jsx
@@ -2,7 +2,7 @@ import Post from "../Post";
 
 function Feed() {
   return (
-    <article className="bg-cream h-[96.5vh] w-full p-2 lg:p-4 rounded-lg overflow-y-scroll">
+    <article className="bg-cream h-[96.5vh] md:w-72 lg:w-full p-2 lg:p-4 rounded-lg overflow-y-scroll">
       <Post />
 
       <Post />
