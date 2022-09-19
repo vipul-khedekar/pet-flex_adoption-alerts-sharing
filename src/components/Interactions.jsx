18818464b80fa4c253aa4ce60f19f56dd0fd3cc2
vipul-khedekar <3kvipul@gmail.com>
2022-09-20 01:44:13 +0530

    Update: feature to like and unlike any post.

--- src/components/Interactions.jsx
+++ src/components/Interactions.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   IoPawOutline,
   IoPaw,
@@ -8,9 +9,25 @@ import {
 function Interactions(props) {
   const { comments } = props;
 
+  const [isLiked, setIsLiked] = useState(false);
+
+  function toggleLike() {
+    setIsLiked(!isLiked);
+  }
+
   return (
     <div className="flex justify-start items-center gap-4 px-2">
-      {<IoPawOutline className="text-xl active:translate-y-1" /> || <IoPaw />}
+      {isLiked ? (
+        <IoPaw
+          onClick={() => toggleLike()}
+          className="text-xl active:translate-y-1"
+        />
+      ) : (
+        <IoPawOutline
+          onClick={() => toggleLike()}
+          className="text-xl active:translate-y-1"
+        />
+      )}
 
       <IoChatbubbleEllipsesOutline className="text-xl active:translate-y-1" />
       <p className="-ml-3 text-xs underline underline-offset-2">
