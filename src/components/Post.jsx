e5bcc8748f4adedbdef8fe6b179c3e51f4a443e1
vipul-khedekar <3kvipul@gmail.com>
2022-09-20 01:22:26 +0530

    Update: Different image heights for mid size and large screens. Consume data and display as each unique post. Bold username and small tag. Body has a bit of spacing from left.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,7 +2,7 @@ import DefaultProfilePic from "../images/profile-picture.jpg";
 import Interactions from "./Interactions";
 
 function Post(props) {
-  const { usernameId, username, tag, body, imageLink, comments } = props.post;
+  const { usernameId, username, tag, body, mediaLink, comments } = props.post;
 
   return (
     <div className="flex flex-col gap-2 mb-5">
@@ -14,25 +14,22 @@ function Post(props) {
         />
 
         <div>
-          <p>Dummy Name</p>
-          <p className="text-sm">@username</p>
+          <p className="font-bold">{username}</p>
+          <p className="text-xs">{`@${tag}`}</p>
         </div>
       </div>
 
-      <div className="flex flex-col items-center justify-start gap-2">
-        <p>
-          Hey, this is my cat. Look at my cat. This is the best cat in the
-          world. Do you like it? Please like and comment!
-        </p>
+      <div className="flex flex-col justify-center items-center gap-2">
+        <p className="self-start ml-2">{body}</p>
 
         <img
-          className="h-40 w-full rounded-lg object-cover object-center"
-          src={DefaultProfilePic}
+          className="h-52 lg:h-72 w-full rounded-lg object-cover object-center"
+          src={mediaLink || DefaultProfilePic}
           alt="content-pic"
         />
       </div>
 
-      <Interactions />
+      <Interactions comments={comments} />
 
       <hr className="border-[0.5px] border-choco" />
     </div>
