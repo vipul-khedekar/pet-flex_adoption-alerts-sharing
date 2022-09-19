c70a5a0c06b9963233f8afb75f7781930c2b05d0
vipul-khedekar <3kvipul@gmail.com>
2022-09-20 02:14:36 +0530

    Fix: Reversed feed to show latest on top. Created fake data.

--- src/components/CenterPanel/Feed.jsx
+++ src/components/CenterPanel/Feed.jsx
@@ -1,12 +1,12 @@
 import Post from "../Post";
 
-import feed from "../../data/feed.json";
+import feed from "../../data/postFeed.json";
 
 function Feed() {
   return (
     <article className="bg-cream h-[96.5vh] md:w-72 lg:w-full p-2 lg:p-4 rounded-lg overflow-y-scroll">
       {feed &&
-        feed.reverse().map((post) => {
+        feed.map((post) => {
           return <Post key={post.id} post={post} />;
         })}
     </article>
