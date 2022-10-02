403e83104ff8cabe7291dede8bb5b9ac22ae6f2a
vipul-khedekar <3kvipul@gmail.com>
2022-10-02 21:02:13 +0530

    Fix: Replaced link with useNavigate. Corrected required form detail questions.

--- src/components/CreateAlertPopup.jsx
+++ src/components/CreateAlertPopup.jsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
 import { IoClose } from "react-icons/io5";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function CreateAlertPopup() {
+  const [petName, setPetName] = useState(``);
+  const [petType, setPetType] = useState(``);
+  const [location, setLocation] = useState(``);
+  const [mediaLink, setMediaLink] = useState(``);
+
+  const navigateTo = useNavigate();
+
+  function handleSubmit() {
+    setPetName(``);
+    setPetType(``);
+    setLocation(``);
+    setMediaLink(``);
+
+    setTimeout(() => {
+      navigateTo(`/`);
+    }, 1200);
+  }
+
   return (
     <aside className="bg-sea h-72 w-[80vw] lg:w-[50vw] flex flex-col gap-4 p-6 rounded-2xl absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-10 modal-shadow">
       <div className="flex justify-between items-center">
@@ -20,7 +39,9 @@ function CreateAlertPopup() {
             </label>
 
             <input
+              onChange={(e) => setPetName(e.target.value)}
               className="bg-cream h-10 w-full p-2 -mt-1 rounded-md text-choco text-xl outline-none"
+              value={petName}
               type="text"
               id="petName"
               placeholder="Pet name"
@@ -29,29 +50,33 @@ function CreateAlertPopup() {
           </div>
 
           <div>
-            <label className="m-2 text-sm" htmlFor="animalType">
-              Animal type
+            <label className="m-2 text-sm" htmlFor="petType">
+              Pet Type
             </label>
 
             <input
+              onChange={(e) => setPetType(e.target.value)}
               className="bg-cream h-10 w-full p-2 -mt-1 rounded-md text-choco text-xl outline-none"
+              value={petType}
               type="text"
-              id="animalType"
-              placeholder="Animal type"
+              id="petType"
+              placeholder="Pet type"
               required
             />
           </div>
 
           <div>
-            <label className="m-2 text-sm" htmlFor="breedType">
-              Breed type
+            <label className="m-2 text-sm" htmlFor="location">
+              Location
             </label>
 
             <input
+              onChange={(e) => setLocation(e.target.value)}
               className="bg-cream h-10 w-full p-2 -mt-1 rounded-md text-choco text-xl outline-none"
+              value={location}
               type="text"
-              id="breedType"
-              placeholder="Breed type"
+              id="Location"
+              placeholder="Location"
               required
             />
           </div>
@@ -63,7 +88,9 @@ function CreateAlertPopup() {
           </label>
 
           <input
+            onChange={(e) => setMediaLink(e.target.value)}
             className="bg-cream h-10 w-full p-2 -mt-1 rounded-md text-choco text-xl outline-none"
+            value={mediaLink}
             type="text"
             id="imageLink"
             placeholder="Image link"
@@ -71,14 +98,15 @@ function CreateAlertPopup() {
           />
         </div>
 
-        <Link to={"/"} className="self-end">
+        <div className="flex justify-between items-center gap-20">
           <button
+            onClick={() => handleSubmit()}
             className="bg-marine h-10 w-40 px-3 py-2 rounded-md text-lg text-sea"
             type="submit"
           >
             Alert
           </button>
-        </Link>
+        </div>
       </form>
     </aside>
   );
