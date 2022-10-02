310a5872116e4f708ec60bd846010ac0950bbe9a
vipul-khedekar <3kvipul@gmail.com>
2022-10-02 23:21:58 +0530

    Update: Feature to do search which can match multiple parameters.

--- src/components/RightPanel/RightPanel.jsx
+++ src/components/RightPanel/RightPanel.jsx
@@ -7,9 +7,23 @@ import AdoptionsList from "../../data/adoptionsList.json";
 function RightPanel() {
   const [adoptionList, setAdoptionList] = useState(AdoptionsList);
 
+  function handleSearch(searchQuery) {
+    if (searchQuery === ``) {
+      return;
+    }
+
+    const searchResult = AdoptionsList.filter((pet) => {
+      return (
+        pet.petName.toLowerCase() === searchQuery.toLowerCase() ||
+        pet.petType.toLowerCase() === searchQuery.toLowerCase()
+      );
+    });
+    setAdoptionList(searchResult);
+  }
+
   return (
     <section className="flex flex-col flex-[3] justify-start items-center gap-4">
-      <SearchBar />
+      <SearchBar handleSearch={handleSearch} />
 
       <AdoptionAlerts adoptionList={adoptionList} />
     </section>
--- src/components/RightPanel/SearchBar.jsx
+++ src/components/RightPanel/SearchBar.jsx
@@ -1,15 +1,25 @@
+import { useState } from "react";
 import { IoMdSearch } from "react-icons/io";
 
-function SearchBar() {
+function SearchBar(props) {
+  const { handleSearch } = props;
+
+  const [searchQuery, setSearchQuery] = useState(``);
+
   return (
     <article className="bg-cream h-14 w-full flex justify-center items-center p-2 rounded-lg relative">
       <input
+        onChange={(e) => setSearchQuery(e.target.value)}
         className="bg-sea w-full p-2 pl-[2.25rem] pr-[5.25rem] rounded-md outline-none text-marine"
+        value={searchQuery}
         type="text"
-        placeholder="Search for specific adoption alerts"
+        placeholder="Search for pet names or types"
       />
 
-      <IoMdSearch className="absolute left-3 z-10 text-2xl" />
+      <IoMdSearch
+        onClick={() => handleSearch(searchQuery)}
+        className="absolute left-3 z-10 text-2xl"
+      />
 
       <button
         className="bg-marine text-sea text-sm px-2 py-1 rounded-md absolute z-10 right-3"
