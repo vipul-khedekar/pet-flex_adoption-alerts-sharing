d31cdcc87c701ef0dd5e47c214d56fcd6b314a9f
vipul-khedekar <3kvipul@proton.me>
2022-12-14 21:14:43 +0530

    Fix: Uneven button when large screen type.

--- src/components/LeftPanel/Poster.jsx
+++ src/components/LeftPanel/Poster.jsx
@@ -4,24 +4,24 @@ import { Link } from "react-router-dom";
 
 function Poster() {
   return (
-    <section className="h-28 w-full bg-cream flex flex-col justify-between items-center gap-3 p-4 rounded-lg island">
+    <section className="h-auto w-full bg-cream flex flex-col justify-between items-center gap-3 p-4 rounded-lg island">
       <Link to={`/home/create-post`}>
         <button
           className="bg-choco flex justify-center items-center gap-2 px-3 py-1 rounded-md text-sunny"
           type="button"
         >
           Post an update{" "}
-          <MdOutlineTipsAndUpdates className="hidden lg:flex text-2xl" />
+          <MdOutlineTipsAndUpdates className="hidden lg:flex text-xl" />
         </button>
       </Link>
 
       <Link to={`/home/create-alert`}>
         <button
-          className="bg-choco flex justify-center items-center gap-2 px-3 py-1 rounded-md text-sunny"
+          className="bg-choco flex justify-center items-center gap-2 px-3 py-1 rounded-md text-base lg:text-sm text-sunny"
           type="button"
         >
           Create an adoption alert{" "}
-          <AiOutlineNotification className="hidden lg:flex text-2xl" />
+          <AiOutlineNotification className="hidden lg:flex text-xl" />
         </button>
       </Link>
     </section>
